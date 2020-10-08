12466c2ac7383bf8d287be2afd3ff2f02140b795
yoichi kobayashi <info@tplh.net>
2020-10-09 00:47:51 +0900

    adjusted the drawer lists.

--- layouts/default.vue
+++ layouts/default.vue
@@ -8,10 +8,46 @@
         |Blog
       v-divider
       v-list
-        v-list-item
-          |Menu
-        v-list-item
-          |Menu
+        v-list-item(
+          link
+          )
+          v-list-item-icon
+            v-icon
+              |mdi-home
+          v-list-item-title
+            |Home
+        v-list-group(
+          no-action
+          )
+          template(
+            v-slot:activator
+            )
+            v-list-item-icon
+              v-icon
+                |mdi-home
+            v-list-item-title
+              |Categories
+          v-list-item(
+            link
+            )
+            v-list-item-title
+              |Menu
+      v-divider
+      v-sheet
+        v-btn(
+          icon
+          href = 'https://github.com/ykob'
+          target = '_blank'
+          )
+          v-icon
+            |mdi-github
+        v-btn(
+          icon
+          href = 'https://twitter.com/ykob0123'
+          target = '_blank'
+          )
+          v-icon
+            |mdi-twitter
     v-main.blue-grey.lighten-5
       nuxt
     Footer
