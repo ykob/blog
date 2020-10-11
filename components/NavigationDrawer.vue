ad3ac78255d5c4aaa1192aa39ee2cbc63825dfd9
yoichi kobayashi <info@tplh.net>
2020-10-11 10:43:03 +0900

    convert Navigation Drawer component to the individual.

---
+++ components/NavigationDrawer.vue
@@ -0,0 +1,53 @@
+<template lang="pug">
+v-navigation-drawer(
+  app
+  )
+  .text-h1
+    |Blog
+  v-divider
+  v-list
+    v-list-item(
+      link
+      )
+      v-list-item-icon
+        v-icon
+          |mdi-home
+      v-list-item-title
+        |Home
+    v-list-group(
+      no-action
+      )
+      template(
+        v-slot:activator
+        )
+        v-list-item-icon
+          v-icon
+            |mdi-label
+        v-list-item-title
+          |Categories
+      v-list-item(
+        link
+        )
+        v-list-item-title
+          |Menu
+  v-divider
+  v-sheet
+    v-btn(
+      icon
+      href = 'https://github.com/ykob'
+      target = '_blank'
+      )
+      v-icon
+        |mdi-github
+    v-btn(
+      icon
+      href = 'https://twitter.com/ykob0123'
+      target = '_blank'
+      )
+      v-icon
+        |mdi-twitter
+</template>
+
+<script>
+export default {}
+</script>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,53 +1,7 @@
 <template lang="pug">
   v-app
     AppBar
-    v-navigation-drawer(
-      app
-      )
-      .text-h1
-        |Blog
-      v-divider
-      v-list
-        v-list-item(
-          link
-          )
-          v-list-item-icon
-            v-icon
-              |mdi-home
-          v-list-item-title
-            |Home
-        v-list-group(
-          no-action
-          )
-          template(
-            v-slot:activator
-            )
-            v-list-item-icon
-              v-icon
-                |mdi-label
-            v-list-item-title
-              |Categories
-          v-list-item(
-            link
-            )
-            v-list-item-title
-              |Menu
-      v-divider
-      v-sheet
-        v-btn(
-          icon
-          href = 'https://github.com/ykob'
-          target = '_blank'
-          )
-          v-icon
-            |mdi-github
-        v-btn(
-          icon
-          href = 'https://twitter.com/ykob0123'
-          target = '_blank'
-          )
-          v-icon
-            |mdi-twitter
+    NavigationDrawer
     v-main.blue-grey.lighten-5
       nuxt
     Footer
@@ -56,11 +10,13 @@
 <script>
 import AppBar from '@/components/AppBar'
 import Footer from '@/components/Footer'
+import NavigationDrawer from '@/components/NavigationDrawer'
 
 export default {
   components: {
     AppBar,
     Footer,
+    NavigationDrawer,
   },
 }
 </script>
