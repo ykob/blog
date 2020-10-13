7b2f3de0bd236d9bccdd52a1cd5697a64fc54900
yoichi kobayashi <info@tplh.net>
2020-10-13 23:04:36 +0900

    adjusted the navigation drawer's layout.

--- components/NavigationDrawer.vue
+++ components/NavigationDrawer.vue
@@ -1,55 +1,65 @@
 <template lang="pug">
 v-navigation-drawer(
   app
+  color = 'blue-grey'
   )
-  .text-h1
-    |Blog
-  v-divider
-  v-list
-    v-list-item(
-      link
-      )
-      v-list-item-icon
-        v-icon
-          |mdi-home
-      v-list-item-title
-        |Home
-    v-list-group(
-      no-action
-      )
-      template(
-        v-slot:activator
-        )
-        v-list-item-icon
-          v-icon
-            |mdi-label
-        v-list-item-title
-          |Categories
+  v-sheet
+    .text-h1.px-3.pt-2.pb-6
+      |Blog
+    v-divider
+    v-list
       v-list-item(
         link
         )
+        v-list-item-icon
+          v-icon
+            |mdi-home
         v-list-item-title
-          |Menu
-    v-list-item
-      v-text-field(
-        label = '記事検索'
-        append-icon = 'mdi-magnify'
+          |Home
+      v-list-group(
+        no-action
         )
-  v-divider
-  v-sheet
+        template(
+          v-slot:activator
+          )
+          v-list-item-icon
+            v-icon
+              |mdi-label
+          v-list-item-title
+            |Categories
+        v-list-item(
+          link
+          )
+          v-list-item-title
+            |Menu
+      v-list-item
+        v-text-field(
+          label = '記事検索'
+          append-icon = 'mdi-magnify'
+          )
+  v-sheet.pa-4(
+    dark
+    color = 'transparent'
+    )
+    p
+      |yoichi kobayashi
     v-btn(
       icon
       href = 'https://github.com/ykob'
       target = '_blank'
       )
-      v-icon
+      v-icon(
+        color = 'white'
+        )
         |mdi-github
     v-btn(
       icon
       href = 'https://twitter.com/ykob0123'
       target = '_blank'
       )
-      v-icon
+      v-icon(
+        color = 'white'
+        )
         |mdi-twitter
 </template>
 
